03c9fc9f8867760bcaabceab0917d0e0c6af8f3b
Lingjia <ll587@cornell.edu>
2024-10-26 20:51:43 -0400

    add objects

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")){
+            isGrounded = true;
+        }
+    }
+}
