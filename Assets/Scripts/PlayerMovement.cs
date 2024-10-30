fed668b53fc5e87e62837934a11098150540f14b
Lingjia <ll587@cornell.edu>
2024-10-29 22:45:27 -0400

    add net and improve movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,34 @@
+using TMPro;
+using Unity.VisualScripting.FullSerializer;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement: MonoBehaviour
 {
-    public float speed = 10f;
-    private Rigidbody rb;
+    private CharacterController _controller;
+    
+    private Vector3 _input;
+    private Vector3 _direction;
+    private float smoothTime = 0.05f;
+    private float _currentVelocity;
+    [SerializeField] private float speed = 5f;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        transform.Translate(movement * speed * Time.deltaTime);
+        _direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (_direction.sqrMagnitude > 0.0f) {
+            float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            _controller.Move(_direction*speed*Time.deltaTime);
+        }
     }
 }
